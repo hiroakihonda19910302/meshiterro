d3706917b57add4138d1e82bc6f79d14d1d85db8
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-08-13 03:37:17 +0900

    [Start] 5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6fb3c4a66e5805a61fb43d58c6bfd3a0bffef3a03f54d084559e1c72e08c382601ed5c599ff0a48042659483b35fdabb04e8095a0ccc25ea7e0bb8797f10aa38'
